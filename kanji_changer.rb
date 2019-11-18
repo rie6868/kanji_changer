dbbce043117b1ca7dab6c5160af3978ee9995fc9
rieyano <rieyano0402@gmail.com>
2019-11-18 11:10:52 +0900

    first commit

---
+++ kanji_changer.rb
@@ -0,0 +1,43 @@
+def num_to_kanji(i)
+  if i < 1 
+    return "error"
+  end
+  kanjis = ["壱","弐","参","四","五","六","七","八","九"]
+  i = i - 1
+  return kanjis[i]
+end
+
+def num_to_kanji_4(i)
+  tmp = i
+  i_1000 = tmp/1000
+  tmp    = tmp%1000
+
+  i_100 = tmp/100
+  tmp   = tmp%100
+
+  i_10 = tmp/10
+  i_1  = tmp%10
+  
+  kanji = ""
+  if i_1000 >= 1 
+    i_1000 = num_to_kanji(i_1000)
+    kanji  = kanji + i_1000 + "千"
+  end
+  if i_100 >= 1
+    i_100  = num_to_kanji(i_100)
+    kanji  = kanji + i_100 + "百"
+  end
+  if i_10 >= 1
+    i_10   = num_to_kanji(i_10)
+    kanji  = kanji + i_10 + "拾"
+  end 
+  if i_1 >= 1
+    i_1    = num_to_kanji(i_1)
+    kanji  = kanji + i_1 
+  end
+  return kanji
+end  
+puts "1から9999の数字を入力してエンターキーを押してください"
+x = gets.to_i
+y = num_to_kanji_4(x)
+puts y
